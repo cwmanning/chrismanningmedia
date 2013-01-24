dba0147cd329bc827a721bbb53cf41c8d50993ae
cwmanning <cwmanning@usatoday.com>
2013-01-23 22:26:12 -0500

    Tweaks some spacing/padding. Beginning to think out interaction.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-    $('.tile').click(function(e){
-        $(this).find('.zoom').toggle();
+    $('.tiles > li > a').click(function(e){
+        //$(this).siblings('.zoom').toggle();
+        var $tile = $(this).parent();
+
+        // if on a small screen, stop here. or come up with alternate animations.
+        // else...
+
+        /** 
+         * Find the other tiles in this row, as well as the next row.
+         * The next row is always lower, unless the current row is the last, then it is higher.
+         */
+
+
+        /**
+        * Animate the two rows in one direction off screen, leaving blank canvas for
+        * larger imagery and project information.
+        */
+
+
         return false;
     });
 });
