877e245aa54f90f85a5a92f1b0eb2a0f40402adb
cwmanning <cwmanning@usatoday.com>
2013-01-24 00:29:05 -0500

    Adds a class to toggle transitions on tiles. Begins setting up javascript to manage the grid.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,40 @@
+function Grid($tiles){
+    this.$tiles = $tiles;
+    this.getRows();
+}
+
+Grid.prototype.navigate = function($tile){
+    this.$active = $tile;
+    console.log($tile.position().top);
+}
+
+Grid.prototype.getRows = function(){
+    var tops = [],
+        seen = [],
+        distinct = [];
+
+    this.$tiles.each(function(index){
+        tops.push($(this).position().top);
+    });
+
+    for(var i=0; i < tops.length; i++){
+        var value = tops[i];
+        if (i === 0 || seen[seen.length - 1] !== value) {
+            seen.push(value);
+            distinct.push(value);
+        }
+    }
+    console.log(distinct);
+    this.rowPositions = distinct;
+}
+
+
 $(document).ready(function(){
-    $('.tiles > li > a').click(function(e){
-        //$(this).siblings('.zoom').toggle();
+    var portfolio = new Grid($('.tiles > li'));
+
+    $('.tiles > li > a').click(function(event){
         var $tile = $(this).parent();
+        portfolio.navigate($tile);
 
         // if on a small screen, stop here. or come up with alternate animations.
         // else...
@@ -17,7 +50,11 @@ $(document).ready(function(){
         * larger imagery and project information.
         */
 
-
         return false;
+    });
+
+    $(window).resize(function(){
+        // throttle this later
+        portfolio.getRows();
     });
 });
