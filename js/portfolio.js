ce4419b7f3f64cb981f92a31bfe1d2317c544356
cwmanning <cwmanning@usatoday.com>
2013-02-01 01:02:02 -0500

    Adds a CSS3 loading indicator (keyframes). Roughly hooks it in to existing JS.

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,31 +8,26 @@
  * @param {object} $tiles The list items to work with.
  */
 function Grid($tiles){
-    $tiles.parent().addClass('ready');
     this.$tiles = $tiles;
     this.$expanded = $('#expanded');
+    this.loadingBars = this.getLoadingBars(12);
     this.getRows();
 }
 
-/** 
- * Show the expanded version of a tile.
- * @param {object} $tile The clicked list item.
+/**
+ * Builds html for CSS3-based loading image.
+ * @param {Number} barCount The number of bars/divs to generate.
  *
- * Clone content and write to a central location.
- * Elements must be positioned outside of the animating tiles,
- * and this allows for that without affecting markup and SEO.
+ * Normally wouldn't do this, but it beats loading in
+ * another javascript file just for templating.
  */
-Grid.prototype.expandTile = function($tile){
-    var more = $tile.find('.more').get(0);
-    if (typeof more !== 'undefined') {
-        var moreClone = more.cloneNode(true);
-        var $img = this.$expanded.html(moreClone).find('img');
-        var dataSrc = $img.attr('data-src');
-        $img.attr('src', dataSrc).load($.proxy(function(){
-            this.$expanded.show();
-        }, this));
+Grid.prototype.getLoadingBars = function(barCount){
+    var loading = '<div class="loading">';
+    for (var i = 1; i <= 12; i++) {
+        loading += '<div class="bar' + i + '"></div>';
     }
-};
+    return loading + '</div>';
+}
 
 /**
  * Calculate and set information about the number of rows in the grid.
@@ -63,6 +58,26 @@ Grid.prototype.getRows = function(){
 };
 
 /** 
+ * Loads the expanded version of a tile.
+ * @param {object} $tile The clicked list item.
+ *
+ * Clone content and write to a central location.
+ * Elements must be positioned outside of the animating tiles,
+ * and this allows for that without affecting markup and SEO.
+ */
+Grid.prototype.loadExpanded = function($tile){
+    var more = $tile.find('.more').get(0);
+    var moreClone = more.cloneNode(true);
+    var $img = this.$expanded.html(moreClone).find('img');
+    var dataSrc = $img.attr('data-src');
+    var dfd = $.Deferred();
+    $img.attr('src', dataSrc).load($.proxy(function(){
+        dfd.resolve();
+    }, this));
+    return dfd.promise();
+};
+
+/** 
  * Navigate between projects (tiles) in the grid.
  * @param {object} $tile The clicked list item.
  *
@@ -71,6 +86,7 @@ Grid.prototype.getRows = function(){
  */
 Grid.prototype.navigate = function($tile){
     this.restore();
+    $tile.append(this.loadingBars);
 
     var rowIndex = Math.floor($tile.index() / this.tilesPerRow);
     var tileStart = rowIndex * this.tilesPerRow;
@@ -79,10 +95,14 @@ Grid.prototype.navigate = function($tile){
         tileStart -= this.tilesPerRow;
     }
     var tileEnd = tileStart + this.tilesMoving;
-    // Animate rows off screen.
-    this.$tiles.slice(tileStart, tileEnd).toggleClass('flying');
 
-    this.expandTile($tile);
+    $.when(this.loadExpanded($tile)).done($.proxy(function(){
+        $tile.find('.loading').remove();
+        // Animate rows off screen.
+        this.$tiles.slice(tileStart, tileEnd).toggleClass('flying');
+        // Reveal the expanded version of the tile.
+        this.showExpanded(rowIndex);
+    }, this));
 };
 
 /** 
@@ -93,9 +113,20 @@ Grid.prototype.restore = function(){
     this.$tiles.removeClass('flying');
 };
 
+/** 
+ * Reveals the expanded version of a tile.
+ * @param {Number} rowIndex The index of the clicked tile's row.
+ *
+ */
+Grid.prototype.showExpanded = function(rowIndex){
+    var rowTop = this.rowPositions[rowIndex];
+    this.$expanded.css('top', rowTop).show();
+};
+
 
 $(window).load(function(){
     var $tiles = $('.tiles > li');
+    $tiles.parent().addClass('ready');
     var portfolio = new Grid($tiles);
 
     $tiles.find('a').click(function(event){
